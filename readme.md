c67afc60e6b321f5bd0acf6929fec8db38c7e238
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-04-21 09:35:48 +0200

    First commit

---
+++ readme.md
@@ -0,0 +1,51 @@
+# personnummer-api
+
+> work in progress
+
+Serverless API to parse and validate and parse a Swedish personal identity numbers. Built for [OpenFaaS](https://www.openfaas.com/).
+
+## docs
+
+```json
+POST /parse
+{
+  "ssn": "198507099805"
+}
+
+{
+  "data": {
+		"valid": true,
+		"age": 34,
+		"female": true,
+		"male": false,
+		"coordination_number": false,
+		"century": "19",
+		"fullYear": "1985",
+		"year": "85",
+		"month": "07",
+		"day": "09",
+		"sep": "-",
+		"num": "980",
+		"check": "5",
+    "format_long": "198507099805",
+    "format_short": "850709-9805"
+  }
+}
+```
+
+```json
+POST /validate
+{
+  "ssn": "198507099805"
+}
+
+{
+  "data": {
+    "valid": true
+  }
+}
+```
+
+## License
+
+MIT
---
+++ src/handler.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const personnummer = require('personnummer');
+
+/**
+ * Base response body.
+ *
+ * @var {object}
+ */
+const baseResBody = {
+  meta: {
+    name: 'personnummer-api',
+    spec: '3.0.0',
+    version: '1.0.0',
+  },
+  data: {},
+};
+
+/**
+ * Convert class to object.
+ *
+ * @param {object} theClass
+ *
+ * @return {object}
+ */
+const classToObject = (theClass) => {
+  const originalClass = theClass || {};
+  const keys = Object.getOwnPropertyNames(Object.getPrototypeOf(originalClass));
+  return keys.reduce((classAsObj, key) => {
+    classAsObj[key] = originalClass[key];
+    return classAsObj;
+  }, {});
+};
+
+module.exports = (event, context) => {
+  let resBody = baseResBody;
+
+  switch (event.path) {
+    case '/parse':
+      try {
+        const p = personnummer.parse(event.body.ssn);
+
+        resBody.data = Object.assign(
+          {
+            age: p.getAge(),
+            coordination_number: p.isCoordinationNumber(),
+            format_long: p.format(true),
+            format_short: p.format(),
+            female: p.isFemale(),
+            male: p.isMale(),
+            valid: true,
+          },
+          classToObject(p)
+        );
+
+        // use snake case.
+        resBody.data.full_year = res.data.fullYear;
+        delete resBody.data.fullYear;
+      } catch (e) {
+        resBody.data.valid = false;
+      }
+      break;
+    case '/validate':
+      resBody = Object.assign(baseResBody, {
+        data: {
+          valid: personnummer.valid(event.body.ssn),
+        },
+      });
+      break;
+    default:
+      break;
+  }
+
+  context
+    .headers({
+      'Content-Type': 'application/json',
+    })
+    .status(200)
+    .succeed(resBody);
+};
